ff2682487e7b7350ae533679dd0e28bce5add09d
Eric Moolin <eric@clsimplex.com>
2021-10-28 11:28:11 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'storage-test',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
